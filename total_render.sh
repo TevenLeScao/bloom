5263ba1c9209b0993fd455a12f748ece5fd5d2c8
TevenLeScao <teven.le-scao@polytechnique.edu>
2019-05-01 17:38:52 -0400

    rendering script

---
+++ total_render.sh
@@ -0,0 +1,16 @@
+cd brick
+emberrender --in="updates.flame" --opencl --verbose --progress --name_enable --sp
+cd ..
+cd fireflies
+emberrender --in="updates.flame" --opencl --verbose --progress --name_enable --sp
+cd ..
+cd kindling
+emberrender --in="updates.flame" --opencl --verbose --progress --name_enable --sp
+cd ..
+cd willowisp
+emberrender --in="updates.flame" --opencl --verbose --progress --name_enable --sp
+cd ..
+cd crucible
+emberrender --in="updates.flame" --opencl --verbose --progress --name_enable --sp
+cd ..
+
