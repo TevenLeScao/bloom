56e783ac5e343a55cfeb3a7009e6ab861f764f1f
TevenLeScao <teven.lescao@gmail.com>
2020-03-27 01:02:27 +0100

    renders all folders automatically + creates the videos

--- total_render.sh
+++ total_render.sh
@@ -1,20 +1,6 @@
-cd inputs
-cd crucible_highres
-emberrender --in="updates.flame" --opencl --verbose --progress --name_enable --sp
-cd ..
-cd ghost
-emberrender --in="updates.flame" --opencl --verbose --progress --name_enable --sp
-cd ..
-cd hearts
-emberrender --in="updates.flame" --opencl --verbose --progress --name_enable --sp
-cd ..
-cd neon
-emberrender --in="updates.flame" --opencl --verbose --progress --name_enable --sp
-cd ..
-cd sharp
-emberrender --in="updates.flame" --opencl --verbose --progress --name_enable --sp
-cd ..
-cd wavy
-emberrender --in="updates.flame" --opencl --verbose --progress --name_enable --sp
-cd ..
-
+shopt -s nullglob
+for dir in inputs/*/
+do
+    emberrender --in="$dir""updates.flame" --opencl --verbose --progress --name_enable --sp
+    ffmpeg -framerate 25 -i "$dir"%03d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p "$dir"output.mp4
+done
